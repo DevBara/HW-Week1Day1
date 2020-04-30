4efeb571a99fc1c0edae5930068b53226dbe4db0
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-30 09:22:14 -0500

    HW with angel(additions)

--- src/multiplyoutput.java
+++ src/multiplyoutput.java
@@ -1,9 +1,22 @@
-public class multiplyoutput {
+import java.util.Scanner;
+
+//public class multiplyoutput {
+//    public static void main(String[] args){
+//        System.out.println(numProduct(100,100));
+//    }
+//    public static int numProduct(int param1, int param2){
+//        return param1 * param2;
+//    }
+//}
+//
+
+//Correct way to take user input in terminal
+public class multiplyoutput{
     public static void main(String[] args){
-        System.out.println(numProduct(100,100));
-    }
-    public static int numProduct(int param1, int param2){
-        return param1 * param2;
+        Scanner in = new Scanner(System.in);
+
+        int firstUserInputInteger = in.nextInt();
+        int secondUserInputInteger = in.nextInt();
+        System.out.println(firstUserInputInteger * secondUserInputInteger);
     }
 }
-
--- src/unitConversion.java
+++ src/unitConversion.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
+//
+//public class unitConversion {
+//    public static void main(String[] args) {
+//        Scanner a=new Scanner(System.in);
+//        int inch = 1000;
+//
+//        double meters= inch * 0.0254;
+//        System.out.println(meters +  "meters");
+//    }
+//}
 
-public class unitConversion {
-    public static void main(String[] args) {
-        Scanner a=new Scanner(System.in);
-        int inch = 1000;
-
-        double meters= inch * 0.0254;
-        System.out.println(meters + "meters");
+//Correct way with scanner class that allows input
+public class unitConversion{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int inchesGivenByUser= in.nextInt();
+        System.out.println(inchesGivenByUser/39.37 + "meters");
     }
 }
